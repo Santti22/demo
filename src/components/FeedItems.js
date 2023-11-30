71547e2e751d4c1b7bc599800521c9d7f633c279
Santti22 <n0koja00@students.oamk.fi>
2023-11-30 07:40:59 +0200

    rssFeed now works, changes to feedItems.js and some CSS adjustments

--- src/components/FeedItems.js
+++ src/components/FeedItems.js
@@ -1,30 +1,46 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-const FeedItems = (indexed) => {
+const FeedItems = ({indexed}) => {
     const connection = 'http://localhost:3001/api/rss-feed'
 
     const [rssFeed, setRssFeed] = useState([])
+    const [loading, setLoading] = useState(true)
+
     useEffect(() => {
         axios.get(connection)
             .then(response => {
-                setRssFeed(response.data.data)
+                const parser = new DOMParser();
+                const xmlDoc = parser.parseFromString(response.data.data, 'text/xml');
+                const items = xmlDoc.querySelectorAll('item');
+                const parsedItems = Array.from(items).map((item) => {
+                    return {
+                        title: item.querySelector('title').textContent,
+                        date: new Date(item.querySelector('pubDate').textContent),
+                    }
+                })
+                setRssFeed(parsedItems)
+                setLoading(false)
             })
             .catch(error => {
                 console.error('Error fetching feed:', error)
+                setLoading(false)
             })        
     }, []);
-
-   /**  const date = rssFeed.map(item => new Date(item.pubDate));
-    const formattedDate = `${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${(date.getFullYear())}`;
-
-    const title = rssFeed.map(item => item.title);
-*/
-    console.log(rssFeed.pubDate)
+    
+    if (loading) {
+        return <div>Loading...</div>
+    }
+    if (rssFeed.length === 0 || indexed >= rssFeed.length) {
+        console.error(`Empty data: ${indexed}`)
+        return <div>Empty data</div>
+    }
+    console.log(rssFeed[0], rssFeed[1])
+    const formattedDate = rssFeed[indexed].date.toLocaleDateString('en-GB');
     return(
             <div key={indexed}>
-                <div>{}</div>
-                <div>{}</div>
+                <div>{formattedDate}</div>
+                <div>{rssFeed[indexed].title}</div>
             </div>
     )
 }
